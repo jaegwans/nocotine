d7ec0a751d6379e2a2e50e19d7a91647416eb9d8
jgkimdeepbio <jgkim@deepbio.co.kr>
2025-06-06 19:27:07 +0900

    feat: 초기 colorset

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  black: "#000000",
+  text: "#222222",
+  greenLightest: "#EEEFE0",
+  greenLighter: "#D1D8BE",
+  greenLight: "#A7C1A8",
+  greenDark: "#819A91",
+};
+
+export default colors;
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,21 +1,54 @@
+import colors from "@/constants/colors";
 import ContentsScreen from "@/screens/Contents/ContentsScreen";
 import HomeScreen from "@/screens/Home/HomeScreen";
 import MapScreen from "@/screens/Map/MapScreen";
 import SettingScreen from "@/screens/Setting/SettingScreen";
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 
 function RootNavigator() {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName: React.ComponentProps<typeof Ionicons>["name"];
+
+          if (route.name === "Home") {
+            iconName = "home";
+          } else if (route.name === "Map") {
+            iconName = "map";
+          } else if (route.name === "Contents") {
+            iconName = "play-circle";
+          } else if (route.name === "Setting") {
+            iconName = "settings";
+          } else {
+            iconName = "ellipse";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: colors.black,
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
       <Tab.Screen
         name="Map"
         component={MapScreen}
-        options={{ headerShown: false }}
+        options={{ headerTitle: "금연센터 맵" }}
       />
-      <Tab.Screen name="Contents" component={ContentsScreen} />
+      <Tab.Screen
+        name="Contents"
+        component={ContentsScreen}
+        options={{
+          headerTitle: "금연 정보",
+        }}
+      />
       <Tab.Screen name="Setting" component={SettingScreen} />
     </Tab.Navigator>
   );
