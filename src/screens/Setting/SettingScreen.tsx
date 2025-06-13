ac5ff3c6fea0b06cd7f3087fefa12d14cc348560
jgkimdeepbio <jgkim@deepbio.co.kr>
2025-06-13 12:49:34 +0900

    feat: myInfo 초기화 버튼 활성화

--- src/screens/Setting/SettingScreen.tsx
+++ src/screens/Setting/SettingScreen.tsx
@@ -1,14 +1,16 @@
 import { RootStackParamList } from "@/navigation/types";
+import { useMyInfoStore } from "@/store/useMyInfoStore";
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { ScrollView } from "react-native";
+import { Alert, ScrollView } from "react-native";
 import { styles } from "./SettingScreen.styles";
 import ListItem from "./components/ListItem";
 
 const SettingScreen = () => {
   const navigation =
     useNavigation<BottomTabNavigationProp<RootStackParamList, "MyInfo">>();
+  const resetMyInfo = useMyInfoStore((state) => state.resetMyInfo);
   return (
     <ScrollView style={styles.container}>
       <ListItem
@@ -21,7 +23,16 @@ const SettingScreen = () => {
       <ListItem
         label="기록 데이터 초기화"
         onPress={() => {
-          console.log("Language selected");
+          Alert.alert("기록 데이터 초기화", "정말로 초기화하시겠습니까?", [
+            { text: "취소", style: "cancel" },
+            {
+              text: "확인",
+              onPress: () => {
+                resetMyInfo();
+                Alert.alert("초기화 완료", "기록 데이터가 초기화되었습니다.");
+              },
+            },
+          ]);
         }}
       />
       <ListItem
