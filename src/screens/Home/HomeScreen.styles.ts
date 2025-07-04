f459b14edbd4a0129ca0a1cd71da69965024f69a
jaegwans <skhu201811140@gmail.com>
2025-07-04 14:37:43 +0900

    feat: Home 금연 정보 미입력 상태 반영

--- src/screens/Home/HomeScreen.styles.ts
+++ src/screens/Home/HomeScreen.styles.ts
@@ -1,16 +1,30 @@
-import colors from "@/constants/colors";
-import { StyleSheet } from "react-native";
+import colors from '@/constants/colors';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.grayLighter,
-  },
-  scrollContainer: {
-    padding: 24,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: colors.grayLighter,
-    gap: 20,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: colors.grayLighter,
+    },
+    scrollContainer: {
+        padding: 24,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: colors.grayLighter,
+        gap: 20,
+    },
+    errorContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.grayLighter,
+        gap: 30,
+    },
+
+    errorText: {
+        fontSize: 20,
+        textAlign: 'center',
+        paddingHorizontal: 20,
+        lineHeight: 30,
+    },
 });
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,40 +1,63 @@
-import { useMyInfoStore } from "@/store/useMyInfoStore";
-import React from "react";
-import { Text } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "./HomeScreen.styles";
-import CigarettesAvoidedCard from "./components/CigarettesAvoidedCard";
-import HealthStatusCard from "./components/HealthStatusCard";
-import LifeGainedCard from "./components/LifeGainedCard";
-import MoneySavedCard from "./components/MoneySavedCard";
-import SmokeFreeDurationCard from "./components/SmokeFreeDurationCard";
+import { useMyInfoStore } from '@/store/useMyInfoStore';
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from './HomeScreen.styles';
+import CigarettesAvoidedCard from './components/CigarettesAvoidedCard';
+import HealthStatusCard from './components/HealthStatusCard';
+import LifeGainedCard from './components/LifeGainedCard';
+import MoneySavedCard from './components/MoneySavedCard';
+import SmokeFreeDurationCard from './components/SmokeFreeDurationCard';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { SettingStackParamList } from '@/navigation/types';
 
 const HomeScreen = () => {
-  const { smokeStartDateAndTime, quitDateAndTime } = useMyInfoStore(
-    (state) => state
-  );
-  if (smokeStartDateAndTime !== null && quitDateAndTime !== null) {
-    return (
-      <SafeAreaView style={styles.container} edges={["top"]}>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <HealthStatusCard />
-          <SmokeFreeDurationCard
-            smokeStartDateAndTime={smokeStartDateAndTime}
-            smokeEndDateAndTime={quitDateAndTime}
-          />
-          <MoneySavedCard
-            smokeStartDateAndTime={smokeStartDateAndTime}
-            smokeEndDateAndTime={quitDateAndTime}
-          />
-          <LifeGainedCard />
-          <CigarettesAvoidedCard />
-        </ScrollView>
-      </SafeAreaView>
+    const { smokeStartDateAndTime, quitDateAndTime } = useMyInfoStore(
+        (state) => state
     );
-  } else {
-    return <Text>금연 정보를 입력해주세요.</Text>;
-  }
+
+    const navigation =
+        useNavigation<StackNavigationProp<SettingStackParamList>>();
+
+    if (smokeStartDateAndTime === null || quitDateAndTime === null) {
+        return (
+            <SafeAreaView style={styles.errorContainer} edges={['top']}>
+                <Text style={styles.errorText}>
+                    금연 초기 입력 정보가 없습니다.{'\n'}
+                    입력 페이지에서 초기 정보를 입력해주세요.
+                </Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('MyInfo');
+                    }}
+                >
+                    <Text style={{ color: 'blue', fontSize: 18 }}>
+                        초기 정보 입력하기
+                    </Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+        );
+    }
+
+    return (
+        <SafeAreaView style={styles.container} edges={['top']}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <HealthStatusCard />
+                <SmokeFreeDurationCard
+                    smokeStartDateAndTime={smokeStartDateAndTime}
+                    smokeEndDateAndTime={quitDateAndTime}
+                />
+                <MoneySavedCard
+                    smokeStartDateAndTime={smokeStartDateAndTime}
+                    smokeEndDateAndTime={quitDateAndTime}
+                />
+                <LifeGainedCard />
+                <CigarettesAvoidedCard />
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default HomeScreen;
